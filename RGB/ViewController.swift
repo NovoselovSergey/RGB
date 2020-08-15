84c8871f66a0cfad34ff8cd9665d8f7821cf18ef
Novoselov Sergey <s.m.novoselov@icloud.com>
2020-08-16 01:42:56 +0300

    Add "Done" button on keyboard

--- RGB/ViewController.swift
+++ RGB/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var viewLabel: UIView!
     @IBOutlet var redLabel: UILabel!
@@ -53,38 +53,35 @@ class ViewController: UIViewController {
         blueLabel.text = String(format: "%.2f", blueSlider.value)
         
         //Setup text filds
+        redTextField.delegate = self
+        
         redTextField.text = String(format: "%.2f", redSlider.value)
         greenTextField.text = String(format: "%.2f", greenSlider.value)
         blueTextField.text = String(format: "%.2f", blueSlider.value)
+        
+        let toolbar = UIToolbar().ToolbarKeyboard(mySelector: #selector(ViewController.dismissKeyboard))
+        
+        redTextField.inputAccessoryView = toolbar
     }
-
     
     @IBAction func sliderAction(_ sender: UISlider) {
         switch sender.tag {
         case 0:
             redLabel.text = String(format: "%.2f", redSlider.value)
             redTextField.text = String(format: "%.2f", redSlider.value)
-            viewLabel.backgroundColor = UIColor.init(red: CGFloat(redSlider.value),
-                                                     green: CGFloat(greenSlider.value),
-                                                     blue: CGFloat(blueSlider.value),
-                                                     alpha: 1)
+            changeColor()
         case 1:
             greenLabel.text = String(format: "%.2f", greenSlider.value)
             greenTextField.text = String(format: "%.2f", greenSlider.value)
-            viewLabel.backgroundColor = UIColor.init(red: CGFloat(redSlider.value),
-                                                     green: CGFloat(greenSlider.value),
-                                                     blue: CGFloat(blueSlider.value),
-                                                     alpha: 1)
+            changeColor()
         case 2:
             blueLabel.text = String(format: "%.2f", blueSlider.value)
             blueTextField.text = String(format: "%.2f", blueSlider.value)
-            viewLabel.backgroundColor = UIColor.init(red: CGFloat(redSlider.value),
-                                                     green: CGFloat(greenSlider.value),
-                                                     blue: CGFloat(blueSlider.value),
-                                                     alpha: 1)
+            changeColor()
         default: break
         }
     }
+    
     @IBAction func redTextFieldAction() {
     }
     @IBAction func greenTextFieldAction() {
@@ -92,6 +89,41 @@ class ViewController: UIViewController {
     @IBAction func blueTextFieldAction() {
     }
     
+    func changeColor () {
+        viewLabel.backgroundColor = UIColor.init(red: CGFloat(redSlider.value),
+                                                 green: CGFloat(greenSlider.value),
+                                                 blue: CGFloat(blueSlider.value),
+                                                 alpha: 1)
+    }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        redTextField.resignFirstResponder()
+        return true
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
 
+extension UIToolbar {
+    func ToolbarKeyboard(mySelector: Selector) -> UIToolbar {
+        
+        let toolBar = UIToolbar()
+        
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor.blue
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done",
+                                         style: UIBarButtonItem.Style.plain,
+                                         target: self,
+                                         action: mySelector)
+        
+        toolBar.setItems([doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        return toolBar
+    }
+}
